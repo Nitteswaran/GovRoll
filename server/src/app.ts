1ff072edb42b8e0a6ab5a7110a993ef9cb51b6bd
Nitteswaran <spnittes@gmail.com>
2025-12-20 05:01:23 +0800

    fixed header

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,11 @@ import { usageService } from './services/usage';
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // File upload setup (memory storage for processing)
